b7c2328f62351498807c140e57754fc905759dea
gaubian <guillaume.aubian@gaubian.xyz>
2020-02-05 05:58:49 +0100

    Added eda.py

---
+++ eda.py
@@ -0,0 +1,25 @@
+from random import *
+from basics import *
+
+def index_biased(tab, n):
+    return choices(list(range(n)), weights = tab)[0]
+
+def generate_biased(P, n):
+    return [index_biased(P[i], n) for i in range(n)]
+
+def heuristic(n, score):
+    P = [[1/n for _ in range(n)] for _ in range(n)]
+    while True:
+        x = generate_biased(P, n)
+        y = generate_biased(P, n)
+        sc_x = score(x)
+        sc_y = score(y)
+        if sc_x < sc_y:
+            x, y = y, x
+            sc_x, sc_y = sc_y, sc_x
+        if sc_x == n:
+            break
+        for i in range(n):
+            P[i][y[i]] /= 2
+            P[i][x[i]] += P[i][y[i]]
+            
