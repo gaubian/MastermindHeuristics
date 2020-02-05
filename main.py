e4c6cf40c604ad2caa566ad5bf1692638fff8646
gaubian <guillaume.aubian@gaubian.xyz>
2020-02-05 05:52:34 +0100

    eda.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import always_inside_backtracking
 import one_plus_one_ea
 import mu_lambda_ea
 import mu_lambda_ga
+import eda
 
 def main():
     print(tester.tester(10, rls.heuristic))
@@ -22,5 +23,6 @@ def main():
     print(tester.tester(10, mu_lambda_ea.heuristic, args=[2, 2, True]))
     print(tester.tester(10, mu_lambda_ea.heuristic, args=[1, 6, False]))
     print(tester.tester(10, mu_lambda_ga.heuristic, args=[2, 2, 0.5]))
+    print(tester.tester(10, eda.heuristic))
 
 main()
