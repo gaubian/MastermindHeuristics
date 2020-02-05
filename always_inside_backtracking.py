e8aa568f4ca0e928ee5d06738697673f49362ff0
gaubian <guillaume.aubian@gaubian.xyz>
2020-02-05 01:30:44 +0100

    Added always_inside_backtracking

---
+++ always_inside_backtracking.py
@@ -0,0 +1,37 @@
+from basics import *
+from random import *
+from math import *
+
+def isok(remaining, capsup):
+    for x in remaining:
+        if x < 0 or x >= capsup:
+            return False
+    return True
+
+def possible(partialsol, remaining, i, history, n, nbhist):
+    if i == n:
+        return True
+    for c in range(n):
+        partialsol[i] = c
+        for j in range(nbhist):
+            if history[j][i] == c:
+                remaining[j] -= 1
+        if isok(remaining, n - i) and possible(partialsol, remaining, i + 1, history, n, nbhist):
+            return True
+        for j in range(nbhist):
+            if history[j][i] == c:
+                remaining[j] += 1
+    return False
+
+
+def heuristic(n, score):
+    history = []
+    while True:
+        tab = n * [0]
+        inputs = list(map(lambda x : x[0], history))
+        remaining = list(map(lambda x : x[1], history))
+        possible(tab, remaining, 0, inputs, n, len(history))
+        sc = score(tab)
+        history.append((tab, sc))
+        if sc == n:
+            break
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import always_inside_mip
 import one_by_one
 import optimal
 import greedy_best
+import always_inside_backtracking
 
 def main():
     print(tester.tester(20, rls.heuristic))
@@ -13,5 +14,6 @@ def main():
     print(tester.tester(20, one_by_one.heuristic))
     print(tester.tester(3, optimal.heuristic))
     print(tester.tester(3, greedy_best.heuristic))
+    print(tester.tester(10, always_inside_backtracking.heuristic))
 
 main()
