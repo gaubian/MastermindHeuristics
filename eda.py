a7d9e06f0e445c7359580ec076714712831cbfbd
gaubian <guillaume.aubian@gaubian.xyz>
2020-02-05 06:52:17 +0100

    Fixed taking into account multiple colours

--- eda.py
+++ eda.py
@@ -1,17 +1,17 @@
 from random import *
 from basics import *
 
-def index_biased(tab, n):
-    return choices(list(range(n)), weights = tab)[0]
+def index_biased(tab, c, n):
+    return choices(list(range(c)), weights = tab)[0]
 
-def generate_biased(P, n):
-    return [index_biased(P[i], n) for i in range(n)]
+def generate_biased(P, c, n):
+    return [index_biased(P[i], c, n) for i in range(n)]
 
 def heuristic(c, n, score):
     P = [[1/c for _ in range(c)] for _ in range(n)]
     while True:
-        x = generate_biased(P, n)
-        y = generate_biased(P, n)
+        x = generate_biased(P, c, n)
+        y = generate_biased(P, c, n)
         sc_x = score(x)
         sc_y = score(y)
         if sc_x < sc_y:
--- main.py
+++ main.py
@@ -12,18 +12,18 @@ import mu_lambda_ga
 import eda
 
 def main():
-    print(tester.tester(10, 10, rls.heuristic))
-    print(tester.tester(10, 10, annealing.heuristic))
+    print(tester.tester(2, 10, rls.heuristic))
+    print(tester.tester(2, 10, annealing.heuristic))
     #print(tester.tester(10, 10, always_inside_mip.heuristic))
-    print(tester.tester(10, 10, one_by_one.heuristic))
+    print(tester.tester(2, 10, one_by_one.heuristic))
     print(tester.tester(3, 3, optimal.heuristic))
     print(tester.tester(3, 3, greedy_best.heuristic))
     #print(tester.tester(10, 10, always_inside_backtracking.heuristic))
-    print(tester.tester(10, 10, one_plus_one_ea.heuristic))
-    print(tester.tester(10, 10, mu_lambda_ea.heuristic, args=[5, 10, True]))
-    print(tester.tester(10, 10, mu_lambda_ea.heuristic, args=[1, 6, False]))
-    print(tester.tester(10, 10, mu_lambda_ga.heuristic, args=[2, 2, 0.5]))
-    print(tester.tester(10, 10, eda.heuristic))
+    print(tester.tester(2, 10, one_plus_one_ea.heuristic))
+    print(tester.tester(2, 10, mu_lambda_ea.heuristic, args=[5, 10, True]))
+    print(tester.tester(2, 10, mu_lambda_ea.heuristic, args=[1, 6, False]))
+    print(tester.tester(2, 10, mu_lambda_ga.heuristic, args=[2, 2, 0.5]))
+    print(tester.tester(2, 10, eda.heuristic))
 
 
 
